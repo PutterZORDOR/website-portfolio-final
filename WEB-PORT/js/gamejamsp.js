e478f4f15fedb288a8c86bba722026171ae90bde
PutterZORDOR <132139589+PutterZORDOR@users.noreply.github.com>
2024-10-06 09:30:12 +0700

    commit

---
+++ WEB-PORT/js/gamejamsp.js
@@ -0,0 +1,22 @@
+
+function goToHome() {
+    window.location.href = "test.html"; 
+}
+function goToProjects() {
+    window.location.href = "3d-monster.html "
+}
+function BackToProjects() {
+    window.location.href = "gamejam4.html"; 
+}
+function confirmfacbook() {
+
+    window.open("https://www.facebook.com/putter.zordor", "_blank");
+}
+function confirminstagram() {
+
+    window.open("https://www.instagram.com/gyoza_pt/", "_blank");
+}
+function confirmtiktok() {
+
+    window.open("https://www.tiktok.com/@putterzordor", "_blank");
+}
